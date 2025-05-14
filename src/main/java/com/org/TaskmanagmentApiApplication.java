dd21b4360e49d0c7f5ff59b1cebdcd8908aff00b
hampcode <devacademyweb@gmail.com>
2025-05-14 12:37:04 -0500

    primer commit

---
+++ src/main/java/com/org/TaskmanagmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagmentApiApplication.class, args);
+    }
+
+}
